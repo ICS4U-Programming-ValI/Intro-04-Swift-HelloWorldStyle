4d943037d317bafcdd1000764f33d7613260a734
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2025-02-07 03:17:40 +0000

    added HelloWorldSwift

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,20 @@
+//
+
+//  HelloWorldStyl.swift
+
+//
+
+//  Created by Val I
+
+//  Created on 2025-02-06
+
+//  Version 1.0
+
+//  Copyright (c) 2025 Val I. All rights reserved.
+
+//
+
+//  Prints yello and one other message.
+// Prints message
+print("Hello, Alex")
+print("I guess you aren't that bad")
